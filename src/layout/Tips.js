6c00b2305557344687a0efa67b4f86ab97979bcf
Sibuls <sebo.orlowski@gmail.com>
2021-02-11 19:57:35 +0100

    tips done

--- src/layout/Tips.js
+++ src/layout/Tips.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from '../components/Title';
+import tip1 from '../images/main/tip1.png';
+import tip2 from '../images/main/tip2.png';
 
 const Tips = () => {
   return (
@@ -10,8 +12,19 @@ const Tips = () => {
         <article className='tips__article'>
           <h3 className='tips__title  '>Jak stworzyć samemu farby do gry?</h3>
           <div className='tips__container'>
-            <div className='tips-box'></div>
-            <div className='tips__content'></div>
+            <div className='tips-box'>
+              <img src={tip1} alt='' />
+            </div>
+            <div className='tips__content'>
+              <p className='text text-articles'>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                ut justo eleifend, tempus lorem et, facilisis nisi. Praesent et
+                eros non nisl iaculis ultricies eget nec odio. Fusce nec
+                efficitur elit, non ullamcorper libero. Quisque consequat sapien
+                in lacus condimentum posuere.
+              </p>
+            </div>
           </div>
         </article>
         <article className='tips__article'>
@@ -19,8 +32,18 @@ const Tips = () => {
             Jak dobrać naboje z farbą do mojego sprzętu?
           </h3>
           <div className='tips__container'>
-            <div className='tips-box'></div>
-            <div className='tips__content'></div>
+            <div className='tips-box'>
+              <img src={tip2} alt='' />
+            </div>
+            <div className='tips__content'>
+              <p className=' text text-articles'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                ut justo eleifend, tempus lorem et, facilisis nisi. Praesent et
+                eros non nisl iaculis ultricies eget nec odio. Fusce nec
+                efficitur elit, non ullamcorper libero. Quisque consequat sapien
+                in lacus condimentum posuere.
+              </p>
+            </div>
           </div>
         </article>
       </div>
